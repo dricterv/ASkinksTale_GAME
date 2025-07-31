33df3411de57228dbfa2c031ae4c4f2f24929891
Dominic <dominic.s.cullen@gmail.com>
2025-07-31 21:42:00 +0800

    Started on Inventory and Inventory UI
    Also added a Button to reset the CameraRation scripts letterbox or pillarbox for debugging/testing in unity editor.

--- Assets/Scripts/Camera/CameraRatio.cs
+++ Assets/Scripts/Camera/CameraRatio.cs
@@ -32,6 +32,7 @@ public class CameraRatio : MonoBehaviour
             rect.y = (1.0f - scaleheight) / 2.0f;
 
             camera.rect = rect;
+            //Position ui?
         }
         else // add pillarbox
         {
@@ -51,6 +52,49 @@ public class CameraRatio : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            // set the desired aspect ratio (the values in this example are
+            // hard-coded for 16:9, but you could make them into public
+            // variables instead so you can set them at design time)
+            float targetaspect = 3.0f / 2.0f;
+
+            // determine the game window's current aspect ratio
+            float windowaspect = (float)Screen.width / (float)Screen.height;
+
+            // current viewport height should be scaled by this amount
+            float scaleheight = windowaspect / targetaspect;
+
+            // obtain camera component so we can modify its viewport
+            Camera camera = GetComponent<Camera>();
+
+            // if scaled height is less than current height, add letterbox
+            if (scaleheight < 1.0f)
+            {
+                Rect rect = camera.rect;
+
+                rect.width = 1.0f;
+                rect.height = scaleheight;
+                rect.x = 0;
+                rect.y = (1.0f - scaleheight) / 2.0f;
+
+                camera.rect = rect;
+
+            }
+            else // add pillarbox
+            {
+                float scalewidth = 1.0f / scaleheight;
+
+                Rect rect = camera.rect;
+
+                rect.width = scalewidth;
+                rect.height = 1.0f;
+                rect.x = (1.0f - scalewidth) / 2.0f;
+                rect.y = 0;
+
+                camera.rect = rect;
+
+            }
+        }
     }
 }
